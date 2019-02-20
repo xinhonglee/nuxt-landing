1cb3988434168d8fc5910146b7590865902e9df6
Andres Pinzon <superdev999@outlook.com>
2019-02-20 19:14:09 +0100

    add contact page

---
+++ pages/contact/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      Contact Page Works !
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
