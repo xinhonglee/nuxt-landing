a5e6d9dd4f451815f761ed68ef7977f0e05c556f
Andres Pinzon <superdev999@outlook.com>
2019-02-26 18:33:08 +0100

    disable proxy

--- api/index.js
+++ api/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const routes = require('./routes');
 
 // Create express instance
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
 // secure apps by setting various HTTP headers
 app.use(helmet());
-
-// Require API routes
-const routes = require('./routes');
 
 // Import API Routes
 app.use(routes);
