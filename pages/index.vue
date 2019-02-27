b46ff1eab91985f7e6e06eb7f515e61df8a8a2a2
xinhonglee <xinhong1010@outlook.com>
2019-02-27 16:09:29 -0500

    Case study component

--- pages/index.vue
+++ pages/index.vue
@@ -9,182 +9,20 @@
   		</div>
   	</section>
     <section class="">
-      <div class="wrap grid-features">
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-speed.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Performance</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-rocket.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Security</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-chip.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Accessibility</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-bulb.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Inspiration</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-target.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Strategy</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-pantone.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Security</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-shirt.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Team Players</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-graph.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Analytics</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-console.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Clean Code</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-vault.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Security</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-browser.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Accessibility</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-tick.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Inspiration</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-help.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Strategy</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-compass.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Inclusion</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-        <article>
-          <div class="icon">
-            <img src="~assets/images/icons/icon-chair.svg" alt="">
-          </div>
-          <div class="content">
-            <h4 class="title">Inclusion</h4>
-            <div class="description">
-              Magnet is an independent web design and production agency, combining art &amp; science with form &amp; function to produce compelling websites.
-            </div>
-          </div><!-- content -->
-        </article>
-      </div>
+      <serviceFeatures/>
     </section>
     <section class="puff-grad-invert">
       <callout/>
+  	</section>
+    <section class="case_studies">
+      <caseUCLA/>
   	</section>
   </main>
 </template>
 
 <script>
-
+  import caseUCLA from '~/components/services/work/case_study_ucla.vue'
+  import serviceFeatures from '~/components/services/features.vue'
   import callout from '~/components/callout.vue'
   import designBlock from '~/components/services/design.vue'
   import engineeringBlock from '~/components/services/engineering.vue'
@@ -192,7 +30,9 @@
     components: {
       callout,
       designBlock,
-      engineeringBlock
+      engineeringBlock,
+      serviceFeatures,
+      caseUCLA
     },
     layout: 'home',
     head: {
