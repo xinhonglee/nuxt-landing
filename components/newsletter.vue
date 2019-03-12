e37acade4c993bd90bf94f7737d25ef239c2b6ad
Andres Pinzon <superdev999@outlook.com>
2019-03-12 21:13:35 +0100

    create newsletter form

---
+++ components/newsletter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="newsletter">
+    <form @submit.prevent="onSubmit">
+      <input class="newsletter-input" type="text" value="">
+    </form>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="text" v-model.trim="$v.contactName.$model" placeholder="">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <input class="input" type="email" v-model.trim="$v.emailAddress.$model" placeholder="">
+      </div>
+    </div>
+    <div class="error-field" v-show="$v.emailAddress.$dirty">
+      <p class="has-text-danger" v-show="!$v.emailAddress.required">Email Address is required</p>
+      <p class="has-text-danger" v-show="!$v.emailAddress.email">Please check the email address was entered
+        correctly.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {required, email} from 'vuelidate/lib/validators';
+
+  export default {
+    name: "newsletter",
+    validations: {
+      emailAddress: {
+        required,
+        email
+      },
+    },
+    methods: {
+      async onSubmit() {
+        this.$v.$touch();
+        if (this.$v.$invalid) {
+          console.log('form validation error-field');
+          return false;
+        }
+        const data = {
+          contactName: this.contactName,
+          emailAddress: this.emailAddress,
+        };
+        const result = await this.$axios.$post('/api/contact/newsletter', data);
+
+        console.log(result);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
