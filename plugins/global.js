40f04507ca5c97527b1eadf933c7a90be426caa9
Andres Pinzon <superdev999@outlook.com>
2019-03-16 17:26:49 +0100

    initialize imageLoaded

--- plugins/global.js
+++ plugins/global.js
@@ -1,4 +1,7 @@
 if (process.browser) {
   window.animejs = require("animejs");
   window.imagesLoaded = require("imagesloaded");
+  imagesLoaded( '#main', function() {
+    console.log('all images are loaded');
+  });
 }
