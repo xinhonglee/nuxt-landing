a1217d35d701a60f3fb2ac71c5c407c2d7663123
Andres Pinzon <superdev999@outlook.com>
2019-03-20 15:12:26 +0100

    imageLoaded

--- plugins/imagesLoaded.js
+++ plugins/imagesLoaded.js
@@ -5,12 +5,19 @@ export default ({app}) => {
      *  Every time the route changes (fired on initialization too)
      */
     app.router.afterEach((to, from) => {
-
       console.log(`initialize images on ${to.fullPath}`);
-      let imgLoad = imagesLoaded('#main');
-      imgLoad.on('always', function () {
-        console.log(imgLoad.images.length + ' images loaded');
-      });
+      setTimeout(() => {
+        const imgLoad = imagesLoaded('#main');
+        imgLoad.on('always', function () {
+          console.log(imgLoad.images.length + ' images loaded');
+        })
+        .on('done', function (instance) {
+          console.log('all images successfully loaded');
+        })
+        .on('fail', function () {
+          console.log('all images loaded, at least one is broken');
+        })
+      }, 100);
     })
   }
 }
